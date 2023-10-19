896e5ef8e3ec7152b5d8f8c4b889542146d83aad
johanindra <124635181+johanindra@users.noreply.github.com>
2023-10-19 10:39:15 +0700

    verifikasi otp

--- srms/verifikasi-otp.php
+++ srms/verifikasi-otp.php
@@ -1,3 +1,6 @@
+<?php
+include("verifikasi.php");
+?>
 <!DOCTYPE html>
 <html lang="id">
 
--- srms/verifikasi.php
+++ srms/verifikasi.php
@@ -2,43 +2,19 @@
 include("../server/koneksi.php");
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = 'admin'; // Tentukan nama pengguna admin yang benar
-    $kode_otp = $_POST['kode_otp'];
-    $otp;
+// Tentukan nama pengguna admin yang benar
+$username = 'admin';
 
-    if (!isset($_SESSION['kode_otp'])) {
-        // Hasilkan OTP acak jika tidak ada dalam sesi
-        $otp = mt_rand(100000, 999999); // Ubah dari 000000 menjadi 100000 untuk OTP 6 digit
-        $_SESSION['kode_otp'] = $otp;
+// Generate OTP
+$otp = mt_rand(100000, 999999);
 
-        // Perbarui kode OTP dalam database untuk pengguna admin yang ditentukan
-        $query = "UPDATE admin SET kode_otp='$otp' WHERE username='$username'";
-        $update = mysqli_query($conn, $query);
-    } else {
-        // Jika OTP sudah ada, gunakan yang ada dalam sesi
-        $otp = $_SESSION['kode_otp'];
-    }
+// Perbarui kode OTP dalam database untuk pengguna admin yang ditentukan
+$query = "UPDATE admin SET kode_otp=:otp WHERE username=:username";
+$statement = $dbh->prepare($query);
 
-    $sql = "SELECT username, kode_otp FROM admin WHERE username='$username'";
-    $select = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($select);
+// Ikat parameter
+$statement->bindParam(':otp', $otp, PDO::PARAM_INT);
+$statement->bindParam(':username', $username, PDO::PARAM_STR);
 
-    if ($row) {
-        $username = $row['username'];
-        $kode = $row['kode_otp'];
-    }
-
-    echo $kode; // Digunakan untuk debug, Anda bisa menghapusnya nanti
-
-    if ($kode_otp == $kode) {
-        header("Location: lupa-password.php");
-        exit();
-    } else {
-        $errorMessage = "Kode OTP salah!!!";
-        echo '<script>';
-        echo 'alert("' . $errorMessage . ' Kode: ' . $kode . '");';
-        echo 'window.location.href = "index.php";';
-        echo '</script>';
-    }
-}
+// Jalankan pernyataan SQL
+$statement->execute();
