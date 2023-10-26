b6eb1b7d669f98ce36b300c7852e2fb4a3b72729
johanindra <124635181+johanindra@users.noreply.github.com>
2023-10-26 12:33:38 +0700

    ganti button

--- srms/laporan-masuk.php
+++ srms/laporan-masuk.php
@@ -200,7 +200,7 @@ if (strlen($_SESSION['alogin']) == "") {
                                                                 <td><?php echo htmlentities(date('d F Y', strtotime($result->tanggal_laporan))); ?></td>
                                                                 <td style="text-align: center;">
                                                                     <a href="detail-laporan.php?NIK=<?php echo htmlentities($result->NIK); ?>">
-                                                                        <img src="img/btn-view.png" alt="Detail" title="Detail Laporan" class="btn-edit-img">
+                                                                        <img src="img/View.png" alt="Detail" title="Detail Laporan" class="btn-edit-img">
                                                                     </a>
                                                                 </td>
                                                             </tr>
